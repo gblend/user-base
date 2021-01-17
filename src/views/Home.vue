93cbfe398a0c8f801c2687baa635778764bdd335
Gabriel Ilochi <you@example.com>
2021-01-17 04:15:21 -1200

    added left and right side containers

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="home"></div>
+  <!-- Left Side Container Begin-->
+  <div class="w-1/2 h-screen md:pl-6 hidden md:block">
+    <div
+      class="flex h-screen w-3/4 mx-auto justify-center items-center flex-col gap-4 text-cst_white-100"
+    ></div>
+  </div>
+  <!-- Left Side Container End-->
+  <!-- Right Side Container Begin -->
+  <div
+    class="w-full md:w-1/2 h-screen bg-cst_white-400 border border-cst_primary-400 rounded-2xl shadow-2xl"
+  >
+    <div class="lg:w-11/12 px-1 mx-auto py-4"></div>
+  </div>
+  <!-- Right Side Container End -->
 </template>
 
 <script>
