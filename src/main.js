f08c8775a27a3151f08caaa4a449682928af7551
Gabriel Ilochi <you@example.com>
2021-01-18 00:15:09 -1200

    imported axios config

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./index.css";
+require("./config-axios");
 
 const app = createApp(App);
 app.use(store);
