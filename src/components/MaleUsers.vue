78ad423f70fe6991d52fefda016bee40e6c4f1e4
Gabriel Ilochi <you@example.com>
2021-01-17 13:59:33 -1200

    created male users component

---
+++ src/components/MaleUsers.vue
@@ -0,0 +1,25 @@
+<template>
+  <users />
+</template>
+
+<script>
+import Users from "./Users";
+import { mapActions, mapGetters } from "vuex";
+import store from "../store";
+export default {
+  name: "MaleUsers",
+  components: {
+    Users,
+  },
+  computed: {
+    ...mapGetters(["categoryName"]),
+  },
+
+  methods: {
+    ...mapActions["SET_CATEGORY_NAME"],
+  },
+  mounted() {
+    store.commit("SET_CATEGORY_NAME", "Male Users");
+  },
+};
+</script>
