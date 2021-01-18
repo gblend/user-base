66c61106e117b2c77cbaa58908c22c47e0a93e5f
Gabriel Ilochi <you@example.com>
2021-01-17 13:56:08 -1200

    added app name and shortname

---
+++ src/components/Country.vue
@@ -0,0 +1,26 @@
+<template>
+  <users />
+</template>
+
+<script>
+import Users from "./Users";
+import { mapActions, mapGetters } from "vuex";
+import store from "../store";
+export default {
+  name: "Countries",
+  components: {
+    Users,
+  },
+  computed: {
+    ...mapGetters(["categoryName"]),
+  },
+
+  methods: {
+    ...mapActions["SET_CATEGORY_NAME"],
+  },
+
+  mounted() {
+    store.commit("SET_CATEGORY_NAME", "User");
+  },
+};
+</script>
