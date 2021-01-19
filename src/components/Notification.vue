7eaefd7e24060132e68d0ce8b3b79851f0139116
Gabriel Ilochi <you@example.com>
2021-01-19 06:13:28 -1200

    created notification component

---
+++ src/components/Notification.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    v-if="notification"
+    class="text-white px-6 py-4 border-0 rounded-2xl relative mb-4 bg-cst_yellow opacity-80"
+  >
+    <span class="text-xl inline-block mr-5 align-middle">
+      <i class="fas fa-bell fa-spin"></i>
+    </span>
+    <span class="inline-block align-middle mr-8 text-sm">
+      <b class="capitalize">Oops!</b> Something went wrong! Request failed.
+    </span>
+    <button
+      class="absolute bg-transparent text-2xl font-semibold leading-none right-0 top-0 mt-4 mr-6 outline-none focus:outline-none"
+      v-on:click="closeAlert()"
+    >
+      <span class="text-cst_beige">×</span>
+    </button>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  name: "Notification",
+  computed: {
+    ...mapState(["notification"]),
+    notification() {
+      return this.$store.state.notification;
+    },
+  },
+  methods: {
+    closeAlert: function () {
+      this.$store.commit("UPDATE_NOTIFICATION_STATUS", false);
+    },
+  },
+};
+</script>
