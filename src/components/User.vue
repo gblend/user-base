aeb6df63cab9041abdc1f6ea1681fc77f0ab4999
Gabriel Ilochi <you@example.com>
2021-01-19 06:25:00 -1200

    added dynamic user details

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,5 +1,5 @@
 <template>
-  <!-- Results  -->
+  <!--   Results  -->
   <div class="self-center pl-3 sm:pl-6 lg:pl-0.5">
     <div class="flex flex-row my-8 ml-1">
       <button type="button" @click="toLastScreen" class="focus:outline-none">
@@ -7,14 +7,18 @@
       </button>
       <h3 class="ml-4 opacity-40">RESULTS</h3>
     </div>
-    <div class="flex w-full py-2 lg:py-0 -ml-5">
+    <div
+      class="flex w-full py-2 lg:py-0 -ml-5"
+      v-for="(userData, index) in user"
+      :key="index"
+    >
       <div class="pr-0.5 lg:pr-5 ml-1 lg:ml-2">
         <div
           class="border-4 lg:border-6 border-cst_teal-200 rounded-full max-h-20 w-20 lg:max-h-40 lg:w-40"
         >
           <img
-            class="block mx-auto sm:mx-0 sm:flex-shrink-0 w-full h-full"
-            src="../../public/img/dog-logo.png"
+            class="block mx-auto sm:mx-0 sm:flex-shrink-0 w-full h-full rounded-full"
+            :src="`${userData?.picture.medium}`"
             alt="User Avatar"
           />
         </div>
@@ -22,13 +26,19 @@
       <div class="w-full flex flex-col sm:text-left mt-2.5">
         <div class="space-y-2">
           <p class="text-xl text-cst_primary-400 font-semibold tracking-wider">
-            Mrs. Shalom Chioma
-            <span class="font-light opacity-60 ml-4">25</span>
+            {{ userData?.name["title"] }}. {{ userData?.name["first"] }}
+            {{ userData?.name["last"] }}
+            <span class="font-light opacity-60 ml-4">{{
+              userData.dob.age
+            }}</span>
           </p>
           <p
             class="text-cst_primary-400 text-opacity-60 tracking-wide font-light text-sm lg:text-base"
           >
-            9278 new road, kilcode, waterford
+            {{ userData.location?.street?.number }}
+            {{ userData.location?.street?.name }},
+            {{ userData.location?.city }},
+            {{ userData.location?.state }}
           </p>
         </div>
         <div
@@ -40,7 +50,7 @@
             >
               <i class="fas fa-envelope text-cst_gray-400 lg:text-base p-2">
               </i>
-              <p>brad.gibson@example.com</p>
+              <p>{{ userData.email }}</p>
             </div>
           </div>
           <div
@@ -49,7 +59,7 @@
             <span
               class="space-x-0.5 lg:space-x-1 flex justify-between items-center rounded-full py-2 px-2.5 bg-cst_beige my-2.5 tracking-tight text-sm font-semibold"
             >
-              <p>JOINED: 2002-05-21</p>
+              <p>JOINED: {{ formatJoinedDate(userData.registered.date) }}</p>
             </span>
           </div>
           <span class="flex space-x-0.5 lg:space-x-1 text-cst_gray-200">
@@ -69,9 +79,9 @@
                 d="M15.05 5A5 5 0 0 1 19 8.95M15.05 1A9 9 0 0 1 23 8.94m-1 7.98v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"
               ></path>
             </svg>
-            <span class="lg:text-sm text-cst_black opacity-30"
-              >011-962-7516</span
-            >
+            <span class="lg:text-sm text-cst_black opacity-30">{{
+              userData.phone
+            }}</span>
           </span>
           <div
             class="flex w-full space-x-1 lg:space-x-4 justify-between items-center"
@@ -83,7 +93,7 @@
               <p
                 class="font-light sm:text-lg text-cst_black opacity-40 lg:tracking-tighter"
               >
-                011-962-7516
+                {{ userData.cell }}
               </p>
             </span>
           </div>
@@ -97,6 +107,7 @@
 <script>
 import { mapActions, mapGetters } from "vuex";
 import store from "../store/index";
+import moment from "moment";
 
 export default {
   name: "User",
@@ -105,13 +116,19 @@ export default {
     toLastScreen() {
       return this.$router.go(-1);
     },
+    formatJoinedDate(date) {
+      return moment(date).format("YYYY-MM-DD");
+    },
   },
   computed: {
-    ...mapGetters(["SET_CATEGORY_NAME"]),
+    ...mapGetters(["SET_CATEGORY_NAME", "user"]),
   },
-
   mounted() {
-    store.commit("SET_CATEGORY_NAME", "User");
+    store.commit("SET_CATEGORY_NAME", "User List");
+  },
+  created() {
+    const id = this.$route.params.userIndex;
+    store.commit("SET_USER", id);
   },
 };
 </script>
